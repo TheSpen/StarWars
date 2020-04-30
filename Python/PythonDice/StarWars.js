5fb5b7f4cf4bb801b81a8fd8d847e3b9c7f82acb
Trevor Plassman <tplassman@wrayward.com>
2020-04-30 15:29:23 -0400

    formatting and updating flow of game slightly

--- Python/PythonDice/StarWars.js
+++ Python/PythonDice/StarWars.js
@@ -1,90 +1,71 @@
+// 1. Build a deck of cards
 const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
-const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; 
-
-
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const deck = [];
-    for (let i = 0; i < suits.length; i++) {
-      for (let j = 0; j < ranks.length; j++) {
-          deck.push({
-              suit: suits[i],
-              rank: ranks[j],
-          });
-        }
-      }
+for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+        deck.push({
+            suit: suits[i],
+            rank: ranks[j],
+        });
+    }
+}
 
-
-//console.log("deck:", deck);
-
-var shuffledDeck = deck
-
-var i = shuffledDeck.length, j, temp;
-while(--i > 0){
-    j = Math.floor(Math.random() * (i+1)); 
+// 2. Shuffle the deck
+const shuffledDeck = deck
+let i = shuffledDeck.length, j, temp;
+while(--i > 0) {
+    j = Math.floor(Math.random() * (i+1));
     temp = shuffledDeck[j];
     shuffledDeck[j] = shuffledDeck[i];
     shuffledDeck[i] = temp;
 }
-//console.log(shuffledDeck)
 
+// 3. Split the Deck
 const midPoint = shuffledDeck.length / 2;
 const rebels = shuffledDeck.slice(0, midPoint);
-const empire = shuffledDeck.slice(midPoint);
-let rebelDeck = rebels
-let empireDeck = empire
+const empires = shuffledDeck.slice(midPoint);
 
 // TODO: Write logic to shift card from each players deck and distribute both cards to winner
-let battleNumber = 1
+function battle(rebels, empires, battleNumber = 1) {
+    const rebelBattleCard = rebels.shift()
+    const empireBattleCard = empires.shift()
 
+    console.log('Battle #' + battleNumber)
+    console.log("The Rebels throw " + rebelBattleCard.rank + " of " + rebelBattleCard.suit)
+    console.log("The Empire throws " + empireBattleCard.rank + " of " + empireBattleCard.suit)
 
-function battle(rebel, empire) {
-  console.log('Battle #' + battleNumber)
-  let rebelBattleCard = rebelDeck.shift()
-  let empireBattleCard = empireDeck.shift()
-  console.log("The Rebels throw " + rebelBattleCard.rank + " of " + rebelBattleCard.suit)
-  console.log("The Empire throws " + empireBattleCard.rank + " of " + empireBattleCard.suit)
-  if (rebelBattleCard.rank > empireBattleCard.rank) {
-      rebelDeck.push(rebelBattleCard)
-      rebelDeck.push(empireBattleCard)
-      console.log('The Rebel Scum won battle #' + battleNumber)
+    if (rebelBattleCard.rank > empireBattleCard.rank) {
+        rebels.push(rebelBattleCard, empireBattleCard)
+        console.log('The Rebel Scum won battle #' + battleNumber)
     } else {
-      empireDeck.push(rebelBattleCard)
-      empireDeck.push(empireBattleCard)
-      console.log('The Evil Empire won battle #' + battleNumber)
+        empires.push(rebelBattleCard, empireBattleCard)
+        console.log('The Evil Empire won battle #' + battleNumber)
     }
-  //console.log(rebelDeck)
-  //console.log(empireDeck)
-  console.log("Rebel Troops: " + rebelDeck.length)
-  console.log("Empire Troops: " + empireDeck.length)
-  console.log("================================")
-  battleNumber++
-}
 
-//console.log(battle())
+    console.log("Rebel Troops: " + rebels.length)
+    console.log("Empire Troops: " + empires.length)
+    console.log("================================")
+}
 
 // TODO: repeat draw logic until one players deck is empty
-function starWars(rebel, empire) {
-  while(rebelDeck.length > 0 && empireDeck.length > 0 && battleNumber <= 1000) {
-    battle(rebelDeck, empireDeck)
-  } if (rebelDeck.length == 0) {
-    console.log("Player Two won the war!")
-  } else if (empireDeck.length == 0) {
-    console.log("Player One won the war!")
-  }
-  console.log("1000 battles were not enough to end this war. The war wages on...")
+function starWars(rebels, empires, maxBattles = 10) {
+    let currentBattle = 0;
+
+    while(rebels.length > 0 && empires.length > 0 && currentBattle++ < maxBattles) {
+        battle(rebels, empires, currentBattle)
+    }
+
+    if (rebels.length == 0) {
+        return "The Empire reigns!";
+    }
+
+    if (empires.length == 0) {
+        return "The Rebels have prevailed!";
+    }
+
+    return maxBattles + " battles were not enough to end this war. The war wages on...";
 }
 
-console.log(starWars(rebelDeck, empireDeck))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 4. Play
+console.log(starWars(rebels, empires));
